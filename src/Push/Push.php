85674db2faf4afe563255839b0b5191bfbe9a1f7
mason <mason.wu@bigins.cn>
2023-09-20 16:28:13 +0800

    push

--- src/Push/Push.php
+++ src/Push/Push.php
@@ -2,7 +2,59 @@
 
 namespace Goatgames\Sdk\Push;
 
+use Goatgames\Sdk\Utils\Helper;
+
 class Push
 {
 
+    // 加密key
+    const PRIVATE_KEY = '';
+    //请求地址
+    const REQ_URL = '';
+
+    public function genSign($gameId, $requestId, $timestamp)
+    {
+        $signStr = implode("", [$gameId, $timestamp, $requestId, self::PRIVATE_KEY]);
+        return md5($signStr);
+    }
+
+    public function sendPush($pushData)
+    {
+        // 获取毫秒
+        $timestamp = time();
+
+        // 获取签名
+        $sign = $this->genSign($pushData['gameId'], $pushData['requestId'], $timestamp);
+
+        // 参数数据
+        $data = [
+            'user_ids'=> $pushData['userIds'],
+            'title' => $pushData['title'],
+            'content' => $pushData['content'],
+        ];
+
+        if (isset($pushData['serverId'])) {
+            $data['server_id'] = $pushData['serverId'];
+        }
+
+        if (isset($pushData['appId'])) {
+            $data['app_id'] = $pushData['appId'];
+        }
+
+        $params = [
+            'type' => $pushData['type'],
+            'data' => json_encode($data)
+        ];
+
+        // header数据
+        $header = [
+            'Content-Type:application/x-www-form-urlencoded;charset=utf-8',
+            'gameId:'.$pushData['gameId'],
+            'requestId:'.$pushData['requestId'],
+            'ts:'.$timestamp,
+            'sign:'.$sign,
+        ];
+
+        return Helper::sendPostRequest(self::REQ_URL, $params, $header);
+    }
 }
--- src/Utils/Helper.php
+++ src/Utils/Helper.php
@@ -1,7 +1,60 @@
 <?php
 
 namespace Goatgames\Sdk\Utils;
+
 class Helper
 {
+    public static function sendPostRequest($url, $params = [], $headers = [], $timeout = 5, $type = "form")
+    {
+        $responseHeaders = [];
 
+        $curl = curl_init();
+        curl_setopt_array($curl, [
+            CURLOPT_URL => $url,
+            CURLOPT_RETURNTRANSFER => true,
+            CURLOPT_ENCODING => '',
+            CURLOPT_MAXREDIRS => 10,
+            CURLOPT_TIMEOUT => $timeout,
+            CURLOPT_FOLLOWLOCATION => true,
+            CURLOPT_HTTP_VERSION => CURL_HTTP_VERSION_1_1,
+            CURLOPT_SSL_VERIFYPEER => false,
+            CURLOPT_SSL_VERIFYHOST => false,
+            CURLOPT_POST => true,
+            CURLOPT_POSTFIELDS => $type == "json" ? json_encode($params, 320) : http_build_query($params),
+            CURLOPT_HTTPHEADER => $headers,
+            CURLOPT_HEADERFUNCTION => function ($curl, $header) use (&$responseHeaders) {
+                $len = strlen($header);
+                $header = explode(':', $header, 2);
+                if (count($header) < 2) {
+                    return $len;
+                }
+
+                $responseHeaders[strtolower(trim($header[0]))][] = trim($header[1]);
+                return $len;
+            }
+        ]);
+
+        if (!$result = curl_exec($curl)) {
+            trigger_error(curl_error($curl));
+        }
+
+        // 获取状态码
+        $httpCode = curl_getinfo($curl, CURLINFO_HTTP_CODE);
+
+        // 关闭连接
+        curl_close($curl);
+
+        return [
+            'response_headers' => array_merge($responseHeaders, [
+                'http_code' => $httpCode
+            ]),
+            'response_body' => $result
+        ];
+    }
+
+    public static function currentmicrotime()
+    {
+        list($s1, $s2) = explode(' ', microtime());
+        return (float)sprintf('%.0f', (floatval($s1) + floatval($s2)) * 1000);
+    }
 }
